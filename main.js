8579d939ac3d29cdb2f8e792ae419506197bb5ea
mahian ahmad <mahianahamd9@gmail.com>
2023-04-12 18:16:51 +0600

    ...

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const searchField = document.getElementById("search-field");
 const searchDropdown = document.getElementById("search-dropdown");
 const menubar = document.getElementById("drawer-navigation");
+const filterBar = document.getElementById("filter-section");
 
 searchField.addEventListener('keypress', () => {
     const searchText = document.querySelector("#search-field input").value;
@@ -21,5 +22,20 @@ function openMenu() {
         menubar.style.transform = 'translateX(0%)';
     } else {
         menubar.style.transform = 'translateX(-100%)';
+    }
+}
+
+// filter section collaps
+function openFilterBar() {
+    if (filterBar.style.transform == 'translateY(100%)') {
+        filterBar.style.transform = 'translateY(0%)';
+        setTimeout(() => {
+            filterBar.style.backgroundColor = '#0000006b';
+        }, 200);
+    } else {
+        filterBar.style.transform = 'translateY(100%)';
+        setTimeout(() => {
+            filterBar.style.backgroundColor = '';
+        }, 200);
     }
 }
